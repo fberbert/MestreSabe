be03084adb091bfbd2217ff395e4f29eff5c3bd3
Fábio Berbert de Paula <fberbert@gmail.com>
2022-09-04 06:21:05 -0300

    teste

--- lambda/index.js
+++ lambda/index.js
@@ -34,16 +34,20 @@ const AskMeIntentHandler = {
       const query = handlerInput.requestEnvelope.request.intent.slots.askme.value
 
       const askOpenAi = async (query) => {
-        const response = await openai.createCompletion({
-          model: "text-davinci-002",
-          prompt: query,
-          temperature: 0,
-          max_tokens: 500,
-          top_p: 1,
-          frequency_penalty: 0.0,
-          presence_penalty: 0.0
-        })
-        return(response.data.choices[0].text)
+        try {
+          const response = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: query,
+            temperature: 0,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0
+          })
+          return response.data.choices[0].text
+        } catch (err) {
+          return 'Estou com preguiça de responder essa pergunta'
+        }
       }
 
       // sair se responder não
