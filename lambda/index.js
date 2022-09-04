a420cd964c45d17013e8148647a366e65260be96
Fábio Berbert de Paula <fberbert@gmail.com>
2022-09-04 06:09:22 -0300

    teste

--- lambda/index.js
+++ lambda/index.js
@@ -38,7 +38,7 @@ const AskMeIntentHandler = {
           model: "text-davinci-002",
           prompt: query,
           temperature: 0,
-          max_tokens: 200,
+          max_tokens: 500,
           top_p: 1,
           frequency_penalty: 0.0,
           presence_penalty: 0.0
