3d1068ed74a5c5cd3dc1389e9b247183b607fa66
Fábio Berbert de Paula <fberbert@gmail.com>
2022-12-17 04:49:19 -0300

    temperature 0.7

--- lambda/index.js
+++ lambda/index.js
@@ -38,7 +38,7 @@ const AskMeIntentHandler = {
           const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: query,
-            temperature: 0,
+            temperature: 0.7,
             max_tokens: 500,
             top_p: 1,
             frequency_penalty: 0.0,
